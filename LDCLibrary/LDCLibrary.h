adb0bc88286ea610b154f14e7c0d8b6d7708c360
vadivelmuniswamy <uvm1cob@rbeigcn.com>
2020-11-20 10:34:04 +0530

    new lib

---
+++ LDCLibrary/LDCLibrary.h
@@ -0,0 +1,19 @@
+//
+//  LDCLibrary.h
+//  LDCLibrary
+//
+//  Created by RBEI on 20/11/20.
+//  Copyright © 2020 foolbuddies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LDCLibrary.
+FOUNDATION_EXPORT double LDCLibraryVersionNumber;
+
+//! Project version string for LDCLibrary.
+FOUNDATION_EXPORT const unsigned char LDCLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LDCLibrary/PublicHeader.h>
+
+
